dd784a4bc80c50a15058d3893df4c4eb8e2d3854
Tóth Péter <pptthh@gmail.com>
2026-05-11 10:35:42 +0200

    update config

--- next-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-import "./.next/0c6465e7-71b9-4deb-89f0-c574f3482d06/dev/types/routes.d.ts";
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
